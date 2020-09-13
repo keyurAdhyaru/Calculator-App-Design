f00b74f2b28591aefac8cee85ce8a9928f093d65
keyur <ka291099@gmail.com>
2020-09-13 20:52:58 +0530

    Code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Calculator-App-Design
+
+
+
+In This Project we are going to build calculator app design.
+
+Watch the Video Tutorial On Youtube.
+
---
+++ app/src/main/java/io/dreamdreamdevelopers/calculator/MainActivity.kt
@@ -0,0 +1,16 @@
+package io.dreamdreamdevelopers.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
